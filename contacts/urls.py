0124fc853d3f5e8c1eecb402de6adaaef5e99d0a
ElimuStuart <stuartelimu@gmail.com>
2019-11-21 03:06:43 +0300

    create contact detail view

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ContactList
+from .views import ContactList, ContactDetail
 
 urlpatterns = [
     path('contacts/', ContactList.as_view(), name='contact-list'),
+    path('contacts/<int:pk>', ContactDetail.as_view(), name='contact-detail'),
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,9 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Contact
 
 class ContactList(ListView):
+    model = Contact
+
+
+class ContactDetail(DetailView):
     model = Contact
