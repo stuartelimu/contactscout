f8ef76c4a0a121e333e70fbc3ce2615152a44cfe
ElimuStuart <stuartelimu@gmail.com>
2019-11-21 04:07:45 +0300

    create detail and delete views

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,9 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Contact
+
 
 class ContactList(ListView):
     model = Contact
@@ -7,3 +11,22 @@ class ContactList(ListView):
 
 class ContactDetail(DetailView):
     model = Contact
+
+
+class ContactCreate(LoginRequiredMixin, CreateView):
+    model = Contact
+    fields = ['first_name', 'last_name', 'telephone', 'email']
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class ContactUpdate(UpdateView):
+    model = Contact
+    fields = ['first_name', 'last_name', 'telephone', 'email']
+
+
+class ContactDelete(DeleteView):
+    model = Contact
+    success_url = reverse_lazy('contact-list')
